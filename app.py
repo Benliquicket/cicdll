22626d9499a7a2b9f0c25858ad795cdca2c28a23
Ben Li <bli@quicketsolutions.com>
2019-08-05 13:35:00 -0500

    sample citation class

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
 
+from modules.citation import Citation
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template("index.html", data="test")
+    citation = Citation(50)
+    citation.pay(40)
+    return render_template("index.html", data=citation.balance)
 
 
 if __name__ == "__main__":
---
+++ modules/citation.py
@@ -0,0 +1,18 @@
+class Citation(object):
+
+    def __init__(self, fine=0):
+        self.fine = fine
+        self.balance = fine
+
+        if not fine:
+            self.status = "no charge"
+        else:
+            self.status = "unpaid"
+
+    def pay(self, amount):
+        self.balance -= amount
+
+        if self.balance:
+            self.status = "partially paid"
+        else:
+            self.status = "paid"
