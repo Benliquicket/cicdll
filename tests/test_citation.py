7f12a17ddce8c9145904515862d90bd3f629c67a
Ben Li <bli@quicketsolutions.com>
2019-08-05 13:46:44 -0500

    sample tests

---
+++ tests/test_citation.py
@@ -0,0 +1,39 @@
+import pytest
+
+from modules.citation import Citation
+
+
+@pytest.fixture
+def no_charge_citation():
+    return Citation()
+
+
+@pytest.fixture
+def charged_citation():
+    return Citation(50)
+
+
+def test_no_charge_citation_constructor(no_charge_citation):
+    assert no_charge_citation.fine == 0
+    assert no_charge_citation.balance == 0
+    assert no_charge_citation.status == "no charge"
+
+
+def test_charged_citation_constructor(charged_citation):
+    assert charged_citation.fine == 50
+    assert charged_citation.balance == 50
+    assert charged_citation.status == "unpaid"
+
+
+def test_charged_citation_partial_payment(charged_citation):
+    charged_citation.pay(30)
+    assert charged_citation.fine == 50
+    assert charged_citation.balance == 20
+    assert charged_citation.status == "partially paid"
+
+
+def test_charged_citation_full_payment(charged_citation):
+    charged_citation.pay(50)
+    assert charged_citation.fine == 50
+    assert charged_citation.balance == 0
+    assert charged_citation.status == "paid"
